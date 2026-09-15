f5b3f193f4fe8d9c461aedcf78e205d5b3f80c7b
vanhoa690 <vanhoa690@gmail.com>
2026-09-15 15:49:32 +0700

    xong lab 1 phan 1

---
+++ lab1.js
@@ -0,0 +1,10 @@
+console.log("day la lab 1");
+// PHP ; $_tenbien
+// JS let, const
+// var: ko dung var
+let name = "hoadv";
+name = "hoadv21";
+const age = 36;
+
+console.log("Ten toi la: ", name);
+console.log("Tuoi cua toi la:", age);
