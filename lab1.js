6b1ce6eaa657d3b4def13fe750a41637a5cea018
vanhoa690 <vanhoa690@gmail.com>
2026-09-17 15:09:23 +0700

    function

--- lab1.js
+++ lab1.js
@@ -1,8 +1,33 @@
 console.log("day la lab 1");
+// PHP ; $_tenbien
+// JS let, const
 
-let name = "hoadv";
+let name = "hoadv"; // string
+let age = 36; // number
+age = "30"; // string
+let myName = "hoadv21"; // string
+// boolean: true / false
+let online = true;
+let isActive = true;
+// Nếu đúng thì xảy ra
+if (isActive) {
+  console.log("hoat dong ", isActive);
+}
+
+// tham trị, tham chiếu, địa chỉ bộ nhớ máy tính
 name = "hoadv21";
-const age = 36;
-
+age = 40;
+const a = 10;
+const b = 3;
+console.log(a * b);
+console.log(a / b);
 console.log("Ten toi la: ", name);
 console.log("Tuoi cua toi la:", age);
+
+// Ham
+function sayHello() {
+  console.log("xin chao");
+  return 10;
+}
+
+sayHello();
