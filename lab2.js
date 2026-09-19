c86f523f57ff282293fd0040871f506e6ed675ad
vanhoa690 <vanhoa690@gmail.com>
2026-09-19 14:43:22 +0700

    lab 2

---
+++ lab2.js
@@ -0,0 +1,8 @@
+// array []
+const student1 = "Nguyễn Văn An";
+const student2 = "Trần Văn Bình";
+const student3 = "Lê Văn Nam";
+
+const students = ["Nguyễn Văn An", "Trần Văn Bình", "Lê Văn Nam"];
+const ids = [1, 5, 23, 45, 54];
+const actives = [true, false, 42, "hoadv21"];
