74d2a6328690b53f6012144f67e1974500ef6a45
vanhoa690 <vanhoa690@gmail.com>
2026-09-24 15:19:05 +0700

    hien thi html

--- lab3.js
+++ lab3.js
@@ -55,24 +55,42 @@ console.log(add2(3, 4));
 console.log(add2(6, 4));
 
 // map
-const students = [
-  { id: 1, name: "An" }, // item
-  { id: 2, name: "Bình" }, // item
-  { id: 3, name: "Cường" }, // item
-];
 
 const mapInfo = students.map((item) => {
   console.log(item);
   return `ID: ${item.id} - Ten: ${item.name}`;
 });
 
-const html = students.map((student) => {
-  return `
-    <tr>
-      <td>${student.id}</td>
-      <td>${student.name}</td>
-    </tr>
-  `;
-});
-console.log(html);
-document.getElementById("app").innerHTML = html;
+const students = [
+  { id: 1, name: "An", age: 22 },
+  { id: 2, name: "Bình", age: 33 },
+  { id: 3, name: "Cường", age: 34 },
+];
+
+document.getElementById("students").innerHTML = students
+  .map(
+    (student) => `
+     <tr class="hover:bg-gray-50">
+              <td class="px-4 py-2 border border-gray-300">${student.id}</td>
+              <td class="px-4 py-2 border border-gray-300">${student.name}</td>
+              <td class="px-4 py-2 border border-gray-300">${student.age}</td>
+              <td class="px-4 py-2 border border-gray-300">
+                <div class="flex items-center justify-center gap-2">
+                  <a
+                    href="#"
+                    class="bg-blue-500 hover:bg-blue-600 text-white px-3 py-1 rounded"
+                  >
+                    Edit
+                  </a>
+
+                  <button
+                    class="bg-red-500 hover:bg-red-600 text-white px-3 py-1 rounded"
+                  >
+                    Delete
+                  </button>
+                </div>
+              </td>
+            </tr>
+            `,
+  )
+  .join("");
